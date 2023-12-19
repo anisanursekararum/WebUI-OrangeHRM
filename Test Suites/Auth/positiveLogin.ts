69903410a7aa17518fd43f93bab8c108c0ca4862
anisanursekararum <anisanursekararum@gmail.com>
2023-12-19 19:44:57 +0700

    add test suite folder

---
+++ Test Suites/Auth/positiveLogin.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>positiveLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>102976f6-b57b-4e24-a86d-87e797a35c04</testSuiteGuid>
+</TestSuiteEntity>
